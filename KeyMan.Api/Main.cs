32f0ba4d419faf70bf3579ea0192afbec407e510
Chris Ballance <github@ballance.it>
2016-07-16 10:02:10 -0400

    Updating reference for LiteDB

---
+++ KeyMan.Api/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyMan.Api
+{
+    public class SampleModule : Nancy.NancyModule
+    {
+        public SampleModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
